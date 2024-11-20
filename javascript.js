cf61bf0f07073a3da4a5701d860cb8df144740e4
Lucas Tanaka <160812489+Chico7854@users.noreply.github.com>
2024-11-19 23:34:37 -0300

    Make the elements of the grid a square
    Before i was a rectangle, now it isnt a X by X grid but they are
    squares now

--- javascript.js
+++ javascript.js
@@ -1,11 +1,11 @@
-function createGrid() {
+function createGrid(numberOfRows) {
     container.innerHTML = '';
 
     for (let i = 0; i < gridSize.dataset.size; i++) {
         const row = document.createElement("div");
         container.appendChild(row);
 
-        for (let j = 0; j < gridSize.dataset.size; j++) {
+        for (let j = 0; j < numberOfRows; j++) {
             const column = document.createElement("div");
             row.appendChild(column);
             column.classList.add("gridSquare");
@@ -18,9 +18,15 @@ function addGridEventListeners() {
 
     grid.forEach(square => {
         square.addEventListener("mouseenter", () => {
-            square.classList.add("backgroundColor");
+            square.style.backgroundColor = "black";
         });
     })
+}
+
+function calculateNumberOfRows(numberOfColumns) {
+    const squareWidth = window.innerWidth / numberOfColumns;
+    const numberOfRows = Math.floor(window.innerHeight / squareWidth);
+    return numberOfRows;
 }
 
 const container = document.querySelector("#container");
@@ -28,6 +34,7 @@ const gridSize = document.querySelector("#size");
 
 gridSize.addEventListener("click", () => {
     gridSize.dataset.size = prompt ();
-    createGrid();
+    let numberOfRows = calculateNumberOfRows(gridSize.dataset.size);
+    createGrid(numberOfRows);
     addGridEventListeners();
 })
